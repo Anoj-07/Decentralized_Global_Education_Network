6adaee5ed2d6f23666188f0f9f2fdc2e5f3cc500
Anoj Rawal <105431901+Anoj-07@users.noreply.github.com>
2024-08-28 16:05:09 +0530

    Delete frontend directory

--- frontend/src/contracts/web3.js
+++
@@ -1,6 +0,0 @@
-import Web3 from 'web3';
-
-// Setup provider (for example, MetaMask or local Ethereum node)
-const web3 = new Web3(window.ethereum || 'http://localhost:8545');
-
-export default web3;
